a352bb556ef3d59f35310f5c52fd506030dc4bf0
Muntiu Silviu-Ioan <silviu-ioan.muntiu@continental-corporation.com>
2025-07-08 09:33:02 +0300

    Initial commit - Animal Sounds project

---
+++ Animal sounds/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_sounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IAnimal> animals = new List<IAnimal>
+            {
+                new Dog(),
+                new Cat(),
+                new Cow(),
+                new Horse(),
+                new Chicken(),
+                new Elephant(),
+                new Snake(),
+                new Sheep()
+            };
+
+            foreach (var animal in animals)
+            {
+                Console.WriteLine($"{animal.GetTypeName()} makes sound: {animal.MakeSound()}");
+            }
+        }
+    }
+}
